21dd940904ca0fa2e65aa5064ae7e70a915e0c82
Mark-H <hamstra.mark@gmail.com>
2011-04-22 02:10:55 +0200

    Rename addon from modImport to importX on Shaun's request

--- assets/components/importx/connector.php
+++ assets/components/importx/connector.php
@@ -20,7 +20,7 @@
  * You should have received a copy of the GNU General Public License along with
  * this program; if not, write to the Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA 02111-1307 USA
- *
+ * 
  */
 require_once dirname(dirname(dirname(dirname(__FILE__)))).'/config.core.php';
 require_once MODX_CORE_PATH.'config/'.MODX_CONFIG_KEY.'.inc.php';
