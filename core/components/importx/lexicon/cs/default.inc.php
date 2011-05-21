f6c6e9ce44912a5ae89aa1fa7b85c1c6e23a58c8
Jan Tezner <jan.tezner@gmail.com>
2011-05-21 21:33:42 +0200

    Czech lexicon update

--- core/components/importx/lexicon/cs/default.inc.php
+++ core/components/importx/lexicon/cs/default.inc.php
@@ -22,9 +22,9 @@
  * Place, Suite 330, Boston, MA 02111-1307 USA
  *
  * @author modxcms.cz
- * @updated 2011-04-25
+ * @updated 2011-05-21
  */
-// Last update: 25/4/2011
+// Last update: 21/5/2011
 // Translator: Hansek
 $_lang['importx'] = 'ImportX';
 $_lang['importx.description'] = 'Import CSV dat jako nové dokumenty';
@@ -37,13 +37,16 @@ $_lang['importx.tab.input'] = 'CSV vstup';
 $_lang['importx.tab.input.desc'] = 'Do pole níže vložte zdrojová data, jednotlivá pole oddělená oddělovačem (výchozí oddělovač je středník) a jednotlivé záznamy každý na novém řádku.';
 $_lang['importx.tab.input.sep'] = 'Pokud ve zdrojových datech používáte jiný oddělovač uveďte ho do tohoto políčka. Políčko ponechte nevyplněné pokud používáte středník.';
 $_lang['importx.csv'] = 'Zdrojová CSV data';
+$_lang['importx.parent'] = 'Rodič:'; // Not parent resource, as this can also be a context!
+$_lang['importx.csvfile'] = 'Nahrátí CSV souboru';
 $_lang['importx.separator'] = 'Oddělovač';
 $_lang['importx.tab.settings'] = 'Výchozí nastavení';
 $_lang['importx.tab.settings.desc'] = 'Zvolte výchozí nastavení, které chcete použít. Tyto volby můžete pro každý záznam přetížit odkazováním se na název pole v CSV datech.';
 $_lang['importx.err.noparent'] = 'Zadejte ID rodiče, pod kterého mají být dokumenty naimportovány. Pokud chcete dokumenty umístit do kořenu portálu zadejte 0.';
-$_lang['importx.err.parentnotnumeric'] = 'Rodič není číslo.';
+$_lang['importx.err.parentnotnumeric'] = 'Rodič není číslo nebo platný klíč kontextu.';
 $_lang['importx.err.parentlessthanzero'] = 'Rodič musí být kladné celé číslo.';
 $_lang['importx.err.nocsv'] = 'Pokud chcete provést import pak zadejte zdrojová CSV data.';
+$_lang['importx.err.fileuploadfailed'] = 'Chyba při čtení nahrátého souboru.';
 $_lang['importx.err.invalidcsv'] = 'Chybná CSV data.';
 $_lang['importx.err.notenoughdata'] = 'Nedostatek zdrojových dat. Byl očekáván alespoň jeden řádek pro hlavičku hodnot a alespoň jeden řádek hodnot.';
 $_lang['importx.err.elementmismatch'] = 'Počet hodnot neodpovídá. Zkontrolujte správnou syntaxi na řádku [[+line]].';
@@ -52,7 +55,8 @@ $_lang['importx.err.invalidheader'] = 'Hlavička hodnot obsahuje jedno nebo víc
 $_lang['importx.err.intexpected'] = '[[+field]] ([[+int]] je očekáváno jako celé číslo)';
 $_lang['importx.err.tvdoesnotexist'] = '[[+field]] (TV s ID [[+id]] nenalezena)';
 $_lang['importx.log.runningpreimport'] = 'Spouštím před-importní testy na zdrojových datech...';
+$_lang['importx.log.fileuploadfound'] = 'Nahrátí CSV souboru přetíží manuální vstup. Soubor: [[+filename]]';
 $_lang['importx.log.preimportclean'] = 'V před-importních testech nebyla nalezena žádná chyba. Připravuji hodnoty na import...';
-$_lang['importx.log.importvaluesclean'] = 'Nenalezena žádná chyba při kontrole importovaných hodnot. Začínám importovat...';
+$_lang['importx.log.importvaluesclean'] = 'Nenalezena žádná chyba při kontrole importovaných hodnot. Nalezeno [[+count]] záznamů. Začínám importovat...';
 $_lang['importx.log.complete'] = 'Import dokončen. Bylo naimportováno [[+count]] dokumentů.';
 ?>
