fd9246298b98cc9ba8a5064a8a9a6c457c7be2fc
Mark Hamstra <hello@markhamstra.com>
2011-12-15 15:58:06 +0100

    Update some languages to indicate we can actually import more than just CSV at a certain stage

--- core/components/importx/lexicon/en/default.inc.php
+++ core/components/importx/lexicon/en/default.inc.php
@@ -31,14 +31,14 @@ $_lang['importx.form.basic'] = 'Basic import';
 $_lang['importx.startbutton'] = 'Start import';
 $_lang['importx.importsuccess'] = 'Succesfully imported resources into MODX.';
 $_lang['importx.importfailure'] = 'Oops, an error occurred while importing your resources.';
-$_lang['importx.tab.input'] = 'CSV Input';
+$_lang['importx.tab.input'] = 'Data Input';
 // Modified 2/5/2011
-$_lang['importx.tab.input.desc'] = 'Paste or upload your CSV input, separating records with a newline and fields with a semi-colon (;) or the separator of your choice specified in the field below.';
+$_lang['importx.tab.input.desc'] = 'Paste or upload your data, separating records with a newline and fields with a semi-colon (;) or the separator of your choice specified in the field below.';
 $_lang['importx.tab.input.sep'] = 'When your CSV formatted entry uses a different separator, you can declare it here. Leave empty to use a semi-colon.';
-$_lang['importx.csv'] = 'Raw CSV:';
+$_lang['importx.csv'] = 'Raw Data:';
 // Added 2/5/2011: (2 items)
 $_lang['importx.parent'] = 'Parent:'; // Not parent resource, as this can also be a context!
-$_lang['importx.csvfile'] = 'CSV file upload';
+$_lang['importx.csvfile'] = 'Data file upload';
 $_lang['importx.separator'] = 'Separator';
 $_lang['importx.tab.settings'] = 'Default Settings';
 $_lang['importx.tab.settings.desc'] = 'Specify the default settings to be used. You may override these per record by referencing the fieldname in your CSV formatted values.';
@@ -46,17 +46,17 @@ $_lang['importx.err.noparent'] = 'Please choose a Parent to import to. Specify 0
 // Modified 2/5/2011:
 $_lang['importx.err.parentnotnumeric'] = 'Parent not numeric or valid context key.';
 $_lang['importx.err.parentlessthanzero'] = 'Parent needs to be a positive integer.';
-$_lang['importx.err.nocsv'] = 'Please add your CSV values in order for them to be processed.';
+$_lang['importx.err.nocsv'] = 'Please add your data in order for them to be processed.';
 // Added 2/5/2011:
 $_lang['importx.err.fileuploadfailed'] = 'Error reading the uploaded file.';
-$_lang['importx.err.invalidcsv'] = 'Invalid CSV value posted.';
+$_lang['importx.err.invalidcsv'] = 'Invalid data posted.';
 $_lang['importx.err.notenoughdata'] = 'Not enough data given. Expecting at least one header row, and one data row.';
 $_lang['importx.err.elementmismatch'] = 'Element count do not match. Please check for correct syntax on line [[+line]].';
 $_lang['importx.err.savefailed'] = 'An unexpected error occurred saving the resource.';
 $_lang['importx.err.invalidheader'] = 'Your header has one or more invalid fieldnames. The invalid fieldname(s) is (are): [[+fields]].';
 $_lang['importx.log.runningpreimport'] = 'Running pre-import tests on submitted data...';
 // Added 2/5/2011:
-$_lang['importx.log.fileuploadfound'] = 'CSV file upload overriding any manual input. Filename: [[+filename]]';
+$_lang['importx.log.fileuploadfound'] = 'Data file upload overriding any manual input. Filename: [[+filename]]';
 $_lang['importx.log.preimportclean'] = 'No errors in pre-import found. Preparing import values...';
 // Modified 2/5/2011:
 $_lang['importx.log.importvaluesclean'] = 'No errors found while checking the import values: [[+count]] items found. Running import...';
