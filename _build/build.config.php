d1f25c28b294556341cafc7f710ffc58fa2b8e56
Mark Hamstra <hello@markhamstra.com>
2014-01-29 13:57:33 +0100

    Build 1.1.1

--- _build/build.config.php
+++ _build/build.config.php
@@ -1,9 +1,2 @@
 <?php
-/**
- * Define the MODX path constants necessary for core installation
- *
- * @package quip
- * @subpackage build
- */
-define('MODX_CORE_PATH', 'C:\wamp\www\revolution\core\\');
-define('MODX_CONFIG_KEY','config');
+require_once(dirname(dirname(__FILE__)).'/config.core.php');
